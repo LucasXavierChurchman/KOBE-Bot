950dac10ac8dcc2fb21458edaa6be5ca6a31e2e0
lucasxavierchurchman <lucaschurchman@gmail.com>
2019-10-29 20:33:40 -0600

    train working

--- src/train.py
+++ src/train.py
@@ -1,8 +1,20 @@
+import random
 import cv2
+import cv2.cv2 as cv2 #extra import gets rid of error warnings
 import numpy as np
 from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.xception import Xception
+from keras.applications.resnet50 import ResNet50
+from keras.layers.core import Dropout, Flatten, Dense
+from keras.layers.pooling import AveragePooling2D
+from keras.optimizers import SGD
+from keras.layers import Input
+from keras.models import Model
 
-def load_images_and_paths(target_labels):
+def load_images_and_labels(target_labels):
     images = []
     labels = []
 
@@ -22,12 +34,76 @@ def load_images_and_paths(target_labels):
 
     images = np.array(images)
     labels = np.array(labels)
+
     return images, labels
 
+def train(images, labels, epochs, savename):
 
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
 
+    X_train, X_test, y_train, y_test = train_test_split(images, labels, 
+                                                        test_size = 0.20,
+                                                        stratify = labels,
+                                                        random_state = 17)
 
+    ImageNet_mean = np.array([ 123.68, 116.779, 103.939 ])
+
+    train_transformations = ImageDataGenerator(
+                        rotation_range=45,
+                        zoom_range=0.25,
+                        width_shift_range=0.2,
+                        height_shift_range=0.2,
+                        shear_range=0.15,
+                        horizontal_flip=True,
+                        fill_mode="wrap") #constant, nearest, reflect or wrap
+
+    validation_transformations = ImageDataGenerator(ImageNet_mean)  
+
+    train_transformations.mean = ImageNet_mean
+    validation_transformations.mean = ImageNet_mean   
+
+    #load transferred learning model. Need to try resnet50 as well
+    transferred_model = Xception(weights = 'imagenet',
+                                include_top = False,
+                                input_tensor= Input(shape=(224, 224, 3)))
+
+    #build rest of model
+    head_model = transferred_model.output
+    head_model = AveragePooling2D(pool_size=(7, 7))(head_model)
+    head_model = Flatten(name="flatten")(head_model)
+    head_model = Dense(512, activation="relu")(head_model)
+    head_model = Dropout(0.5)(head_model)
+    head_model = Dense(len(lb.classes_), activation="softmax")(head_model)
+
+    #freeze layers from Xception model
+    for layer in transferred_model.layers:
+	    layer.trainable = False
+
+    model = Model(inputs=transferred_model.input, outputs=head_model)
+
+    # opt = SGD(lr=0.0001, momentum=0.9, decay=0.0001)
+    model.compile(loss="binary_crossentropy", optimizer='adam', metrics=["accuracy"])
+
+    #need 2 column target for target
+    y_train = np.hstack((y_train, 1-y_train))
+
+    #gotta figure out why this wont work
+    # H = model.fit_generator(
+    #                         train_transformations.flow(X_train, y_train, batch_size=32),
+    #                         steps_per_epoch=len(X_train) // 32,
+    #                         validation_data=validation_transformations.flow(X_test, y_test),
+    #                         validation_steps=len(X_test) // 32,
+    #                         epochs=epochs)
+
+    model.fit(X_train, y_train, epochs = epochs)
+
+    model.save('{}_{}_epochs.model'.format(savename, epochs))
+
+    return model
+ 
 if __name__ == '__main__':
-
-    labels = ['dunk', 'jumpshot']
-    images, labels = load_images_and_paths(labels)
+    random.seed(17)
+    target_labels = ['dunk', 'jumpshot']
+    images, labels = load_images_and_labels(target_labels)
+    model = train(images, labels, epochs = 10, savename = 'dunk_v_shot')
