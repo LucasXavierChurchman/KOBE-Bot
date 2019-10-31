8e3cc9259d28fedb06e71b481e1cd80d439c3b26
lucasxavierchurchman <lucaschurchman@gmail.com>
2019-10-30 20:34:29 -0600

    preliminary image prediciton

--- src/predict_image.py
+++ src/predict_image.py
@@ -4,15 +4,17 @@ import cv2
 
 model = load_model('dunk_v_shot_15_epochs.model')
 
-img = cv2.imread('/home/lucas/Galvanize/Projects/Capstone-3/src/data/test_dunk/2.derrick-jones-jr-dunk-ahs.jpg')
-# cv2.imshow('dunk', img)
-# cv2.waitKey()
+img_path = '/home/lucas/Galvanize/Projects/Capstone-3/src/data/test_jumpshot/5.Three_point_shoot.JPG'
+ 
+img = cv2.imread(img_path)
+cv2.imshow('dunk', img)
+cv2.waitKey()
 
 mean = np.array([123.68, 116.779, 103.939][::1], dtype="float32")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = cv2.resize(img, (224, 224)).astype("float32")
 img -= mean
 
-# pred = model.predict(np.expand_dims(img, axis=0))[0]
+pred = model.predict(np.expand_dims(img, axis=0))[0]
 
-# print(pred)
+print(pred)
