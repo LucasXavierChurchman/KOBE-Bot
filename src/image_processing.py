5529e1301c0975dae6cb46fb36130ef5f408bc51
LucasXavierChurchman <lucasxavierchurchman@gmail.com>
2019-11-04 10:34:06 -0700

    new image processing class

---
+++ src/image_processing.py
@@ -0,0 +1,33 @@
+import os
+from skimage.io import imread, imread_collection
+from skimage.transform import resize
+from skimage.color import rgb2gray, rgba2rgb, gray2rgb
+from tempfile import TemporaryFile
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_image(path):
+    img = imread(path, plugin='matplotlib')
+    img = resize(img, (240,240))
+    return img
+
+def get_all_images(folder_path):
+    image_list = []
+    for filename in os.listdir(folder_path):
+        print(filename)
+        img = get_image(os.path.join(folder_path, filename))
+        image_list.append(img)
+    return image_list
+
+def images_to_array(image_list, save_name):
+    for n, img in enumerate(image_list):
+        if len(img.shape) == 2: #if grayscale convery to rgb
+            # image_list[n] == gray2rgb(img)
+            image_list.pop(n) #gray2rgb is deprecated and can't find a solutuion. Will pop in the meantime
+        if len(img.shape) == 3 and img.shape[2] == 4: #if rgba convert to rgb
+            image_list[n] = rgba2rgb(img)
+    image_array = np.array(image_list)
+    np.save('../data/{}'.format(save_name), image_array)
+    return image_array
--- src/plots.py
+++ src/plots.py
@@ -6,32 +6,7 @@ from tempfile import TemporaryFile
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
-def get_image(path):
-    img = imread(path, plugin='matplotlib')
-    img = resize(img, (240,240))
-    return img
-
-def get_all_images(folder_path):
-    image_list = []
-    for filename in os.listdir(folder_path):
-        print(filename)
-        img = get_image(os.path.join(folder_path, filename))
-        image_list.append(img)
-    return image_list
-
-def images_to_array(image_list, save_name):
-    for n, img in enumerate(image_list):
-        if len(img.shape) == 2: #if grayscale convery to rgb
-            # image_list[n] == gray2rgb(img)
-            image_list.pop(n) #gray2rgb is deprecated and can't find a solutuion. Will pop in the meantime
-
-        if len(img.shape) == 3 and img.shape[2] == 4: #if rgba convert to rgb
-            image_list[n] = rgba2rgb(img)
-    image_array = np.array(image_list)
-    np.save('../data/{}'.format(save_name), image_array)
-    return image_array
-
+from image_processing import get_all_images, get_image, images_to_array
 
 def plot_image_color(v, ax, resolution, title):
     img = resize(v, (resolution, resolution))
@@ -57,7 +32,7 @@ def plot_image_gray(v, ax, resolution, title, heatmap = False):
         return ax
 
 def plot_processing_demo(image_path, ax_color, ax_heatmap, ax_gray, title):
-    resolutions = [240, 50, 25]
+    resolutions = [240, 120, 60]
     img = get_image(image_path)
     img = np.array(img)
     ax_color = plot_image_color(img, 
@@ -77,7 +52,7 @@ def plot_processing_demo(image_path, ax_color, ax_heatmap, ax_gray, title):
 
 def plot_avgs(image_array_path, ax_color, ax_heatmap, ax_gray, category):
 
-    resolutions = [240, 50, 25]
+    resolutions = [240, 120, 60]
     image_array = np.load(image_array_path)
     avg_img = image_array.mean(axis = 0)
 
