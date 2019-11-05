72ccc653cc179b75722f57b2083b47ed18919be1
lucasxavierchurchman <lucaschurchman@gmail.com>
2019-11-05 15:00:37 -0700

    restructured clip downloading scripts and paths

--- src/download_google_imgs.py
+++ src/download_google_imgs.py
@@ -1,15 +1,19 @@
-from google_images_download import google_images_download   #importing the library
+from google_images_download import google_images_download  # importing the library
 
-response = google_images_download.googleimagesdownload()   #class instantiation
+def download_images():
+    response = google_images_download.googleimagesdownload()   #class instantiation
 
-# the 'jump shot' keyword has a space to avoid downloading the fortnite skin images
-# the directory is later changed to 'jumpshot' manually
-arguments = {'keywords':'dunk, jump shot',
-            'limit': 510,
-            'print_urls':True,
-            'output_directory': '../data/google_imgs/',
-            'format': 'jpg',
-            'type': 'photo'}   #creating list of arguments
+    # the 'jump shot' keyword has a space to avoid downloading the fortnite skin images
+    # the directory is later changed to 'jumpshot' manually
+    arguments = {'keywords':'dunk, jump shot',
+                'limit': 510,
+                'print_urls':True,
+                'output_directory': '../data/google_imgs/',
+                'format': 'jpg',
+                'type': 'photo'}   #creating list of arguments
 
-paths = response.download(arguments)   #passing the arguments to the function
-print(paths)
+    paths = response.download(arguments)   #passing the arguments to the function
+    print(paths)
+
+if __name__ == '__main__':
+    download_images()
