d258d6010d81c6fd67f7abab433d0d535cc24f36
lucasxavierchurchman <lucaschurchman@gmail.com>
2019-11-06 15:08:05 -0700

    denver dunk frames

--- src/extract_frames.py
+++ src/extract_frames.py
@@ -1,25 +1,36 @@
 import cv2
+import os
+import shutil
 
 def extract_frames(type, clip_number):
   '''
-  Splits a video into its individual frames. Writes the frames as images outside 
-  this project directory since it generates tons of images, most of which we 
-  dont actually use.
+  Writes individual frames of clips into /data/broadcast_imgs/temp_frames'
 
-  The images we do care about get copied manually into /data/broadcast_images
+  Erases current contents of /data/broadcast_imgs/temp_frames' each time the function
+  is executed
   '''
-  # clip = '../data/clips/{}/{}_{}.mp4'.format(type, type, clip_number)
-  clip = '../data/clips/{}/{}_{}.mp4'.format(type, type, clip_number)
 
-  print(clip)
+  #erase current contents of temp folder
+  frame_folder = '../data/broadcast_imgs/temp_frames'
+  for the_file in os.listdir(frame_folder):
+    file_path = os.path.join(frame_folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(e)
+
+  #extract frames of clip
+  clip = '../data/clips/{}/{}_{}.mp4'.format(type, type, clip_number)
   vidcap = cv2.VideoCapture(clip)
   success,image = vidcap.read()
   count = 0
   success = True
   while success:
     success, image = vidcap.read()
-    save_dir = "/home/lucas/Pictures/{}_frames/{}_{}_frame_{}.jpg"
-    cv2.imwrite(save_dir.format(type, type, clip_number, count), image)
+    save_dir = '/home/lucas/Pictures/{}_frames/{}_{}_frame_{}.jpg'
+    save_path = frame_folder + '/{}_{}_frame_{}.jpg'
+    cv2.imwrite(save_path.format(type, clip_number, count), image)
     if cv2.waitKey(10) == 27:  # exit if Escape is hit
         break
     count += 1
