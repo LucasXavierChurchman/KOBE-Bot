9e655d1453a99d58471fff753e67f1322e5dfd3f
lucasxavierchurchman <lucaschurchman@gmail.com>
2019-11-06 18:55:17 -0700

    making gridsearch

--- src/grid_search.py
+++ src/grid_search.py
@@ -14,12 +14,15 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasClassifier
 
 
 def load_images_and_labels(target_labels):
     '''
     Loads all images from directories with names in target_labels
     a label and image array
+
     TO DO: Make this work with np.load() and skimage (if possible)
     '''
     images = []
@@ -44,16 +47,13 @@ def load_images_and_labels(target_labels):
 
     return images, labels
 
-def train_CNN(images, labels, epochs, savename):
+def build_CNN(labels, optimizer = 'Adam'):
 
     lb = LabelBinarizer()
     labels = lb.fit_transform(labels)
 
-    X_train, X_test, y_train, y_test = train_test_split(images, labels, 
-                                                        test_size = 0.20,
-                                                        stratify = labels,
-                                                        random_state = 17)
 
+                                                    
     ImageNet_mean = np.array([ 123.68, 116.779, 103.939 ])
 
     train_transformations = ImageDataGenerator(
@@ -70,7 +70,7 @@ def train_CNN(images, labels, epochs, savename):
     train_transformations.mean = ImageNet_mean
     validation_transformations.mean = ImageNet_mean   
 
-    #load transferred learning model. Need to try resnet50 as well
+    #load transferred learning model
     transferred_model = ResNet50(weights = 'imagenet',
                                 include_top = False,
                                 input_tensor= Input(shape=(240, 240, 3)))
@@ -89,32 +89,52 @@ def train_CNN(images, labels, epochs, savename):
     for layer in transferred_model.layers:
 	    layer.trainable = False
 
-    opt = SGD(lr=0.0001, momentum=0.9, decay=0.0001)
-    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-
-    #need 2 column array for target
-    y_train = np.hstack((y_train, 1-y_train))
-    y_test = np.hstack((y_test, 1-y_test))
-
-    #gotta figure out why this wont work
-    # H = model.fit_generator(
-    #                         train_transformations.flow(X_train, y_train, batch_size=32),
-    #                         steps_per_epoch=len(X_train) // 32,
-    #                         validation_data=validation_transformations.flow(X_test, y_test),
-    #                         validation_steps=len(X_test) // 32,
-    #                         epochs=epochs)
-
-    model.fit(train_transformations.flow(X_train, y_train), 
-            validation_data = (X_test, y_test),
-            epochs = epochs)
+    model.compile(loss="binary_crossentropy", 
+                optimizer= optimizer, 
+                metrics=["accuracy"])
 
     return model
- 
-if __name__ == '__main__':
-    random.seed(17)
-    savename = 'dunk_v_shot'
-    epochs = 10
-    target_labels = ['dunk', 'jumpshot']
-    images, labels = load_images_and_labels(target_labels)
-    model = train_CNN(images, labels, epochs = epochs, savename = savename)
-    model.save('../models/{}_{}_epochs_grayscale.model'.format(savename, epochs))
+
+target_labels = ['dunk', 'jumpshot']
+images, labels = load_images_and_labels(target_labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+
+random.seed(17)
+train_transformations = ImageDataGenerator(
+                rotation_range=45,
+                zoom_range=0.25,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.15,
+                horizontal_flip=True,
+                fill_mode="wrap")
+
+X_train, X_test, y_train, y_test = train_test_split(images, labels, 
+                                                test_size = 0.20,
+                                                stratify = labels,
+                                                random_state = 17)
+
+#need 2 column array for target
+y_train = np.hstack((y_train, 1-y_train))
+y_test = np.hstack((y_test, 1-y_test))
+
+model = KerasClassifier(build_fn=build_CNN(labels = labels), verbose = 0)
+
+batch_size = [10]#, 20, 40, 60, 80, 100]
+epochs = [10]#, 25, 50, 100]
+optimizer = ['SGD']#, 'Adam', 'RMSprop']
+param_grid = dict(batch_size = batch_size, epochs = epochs)#, optimizer = optimizer)
+
+grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = 3, scoring = 'accuracy')
+
+grid_result = grid.fit(X_train, y_train, validation_data = (X_test, y_test))
+
+print('Best: {} using {}'.format(grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("{} ({}) with: {}".format(mean, stdev, param))
+
